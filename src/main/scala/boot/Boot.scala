902581ce60e50a097a41d83d16bbcb5ed1eeae3b
Brian Hsu <brianhsu.hsu@gmail.com>
2015-11-28 08:26:20 +0800

    Add basic check routine.

--- src/main/scala/boot/Boot.scala
+++ src/main/scala/boot/Boot.scala
@@ -4,6 +4,9 @@ import net.liftweb.http._
 import net.liftweb.http.LiftRules
 import net.liftweb.http.Req
 import net.liftweb.util.Props.RunModes
+import net.liftweb.sitemap._
+import net.liftweb.sitemap.Loc.EarlyResponse
+import scala.util._
 
 class Boot 
 {
@@ -14,11 +17,54 @@ class Boot
     XmlResponse(content.head, code, "text/html", req.cookies)
   }
 
+  def redirectToXG = EarlyResponse { () =>
+    val connectionTry = Try {
+      val httpConnection = new java.net.URL("http://221.4.141.146")
+                              .openConnection()
+                              .asInstanceOf[sun.net.www.protocol.http.HttpURLConnection]
+
+      httpConnection.setConnectTimeout(60 * 1000)
+      httpConnection.setReadTimeout(60 * 1000)
+      httpConnection.getResponseCode
+    }
+
+    connectionTry match {
+      case Success(200) => S.redirectTo("http://221.4.141.146")
+      case _            => S.redirectTo("http://dashboardXG.zhenhai.com.tw")
+    }
+  }
+
+  def redirectToSZ = EarlyResponse { () =>
+
+    val connectionTry = Try {
+
+      val httpConnection = new java.net.URL("http://218.4.250.102")
+                              .openConnection()
+                              .asInstanceOf[sun.net.www.protocol.http.HttpURLConnection]
+
+      httpConnection.setConnectTimeout(60 * 1000)
+      httpConnection.setReadTimeout(60 * 1000)
+      httpConnection.getResponseCode
+    }
+
+    connectionTry match {
+      case Success(200) => S.redirectTo("http://221.4.141.146")
+      case _            => S.redirectTo("http://dashboardXG.zhenhai.com.tw")
+    }
+  }
+
   def boot 
   {
+    val siteMap = SiteMap(
+      Menu("Home") / "index",
+      Menu("RedirectToXG") / "redirectToXG" >> redirectToXG,
+      Menu("RedirectToSZ") / "redirectToSZ" >> redirectToSZ
+    )
+
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
     LiftRules.addToPackages("code")
+    LiftRules.setSiteMap(siteMap)
 
     LiftRules.exceptionHandler.prepend {
       case (runMode, req, exception) if runMode == RunModes.Production =>
